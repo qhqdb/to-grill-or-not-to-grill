25c3229cbd04c6c325f22a5a0cce6a8838777b85
Michael Ha <mic.ha.el@me.com>
2022-07-07 22:38:13 -0700

    html + tailwindcss

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+var apikey = "&appid=fd2f5b31e512a63b6592c6edcefe3002";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=";
